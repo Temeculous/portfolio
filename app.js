8c872c2e47c353049b03299441a956fec8898e0b
temeculous <zacerious@gmail.com>
2023-06-06 12:06:26 -0400

    added footer links and collapsible nav sidebar

---
+++ app.js
@@ -0,0 +1,13 @@
+$(window).on("load", function () {
+  const openNav = function () {
+    $("#nav").css("width", "250px");
+    $("#main").css("marginLeft", "250px");
+  };
+
+  const closeNav = function () {
+    $("#nav").css("width", "0");
+    $("#main").css("marginLeft", "0");
+  };
+  $(".closebtn").on("click", closeNav);
+  $(".openbtn").on("click", openNav);
+});
