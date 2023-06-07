669099e597fda5bfd2f0f0a81165a8f44d00568d
temeculous <zacerious@gmail.com>
2023-06-07 13:16:42 -0400

    Added modal to title on resume page

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 $(window).on("load", function () {
-
   const openNav = function () {
     $("#nav").css("width", "250px");
     $("#main").css("marginLeft", "250px");
@@ -10,6 +9,22 @@ $(window).on("load", function () {
     $("#main").css("marginLeft", "0");
   };
 
+  const tmodal = $(".title-modal");
+  const modalOpen = $("#modal-btn");
+  const modalClose = $(".close");
+
+  modalOpen.on("click", function () {
+    tmodal.css("display", "block");
+  });
+  modalClose.on("click", function () {
+    tmodal.css("display", "none");
+  });
+
+  $(window).on("click", function (e) {
+    if (e.target == tmodal) {
+      tmodal.css("display", "none");
+    }
+  });
   $(".closebtn").on("click", closeNav);
   $(".openbtn").on("click", openNav);
 });
